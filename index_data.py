21a657f33443f9626aa1c8d2d3e6c85af4e2c975
kousikgvs <kousikgvs@gmail.com>
2026-04-01 02:44:22 +0000

    added index_data and query_rag into 2 different files from the notebook code to maintain easy Training and Rag inference

--- index_data.py
+++ index_data.py
@@ -1,1 +1,67 @@
-# RAG indexing of PDFS here.
+
+import os
+from dotenv import load_dotenv
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance, PointStruct
+
+# Load environment variables
+load_dotenv(override=True)
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+QUADRANT_URL = os.getenv("QUADRANT_URL")
+QUADRANT_API_KEY = os.getenv("QUADRANT_API_KEY")
+
+# Only keep post title, headers, and content from the full HTML.
+bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs4_strainer},
+)
+docs = loader.load()
+
+# Split the documents into chunks
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100,
+)
+chunks = splitter.split_documents(docs)
+texts = [doc.page_content for doc in chunks]
+
+# Load the sentence transformer model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Create vectors
+vectors = model.encode(texts)
+
+# Connect to Qdrant
+qdrant_client = QdrantClient(
+    url=QUADRANT_URL,
+    api_key=QUADRANT_API_KEY,
+)
+
+# Create collection
+#qdrant_client.recreate_collection(
+#    collection_name="nutrition_rag",
+#    vectors_config=VectorParams(size=384, distance=Distance.COSINE),
+#)
+
+# Create points
+points = []
+for i, (text, vector) in enumerate(zip(texts, vectors)):
+    points.append(
+        PointStruct(
+            id=i,
+            vector=vector.tolist(),  # convert numpy → list
+            payload={"text": text}   # store original chunk
+        )
+    )
+
+# Upsert points
+qdrant_client.upsert(
+    collection_name="nutrition_rag",
+    points=points
+)
--- query_rag.py
+++ query_rag.py
@@ -1,1 +1,48 @@
-# RAG Quering Here.
+
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage, HumanMessage
+from sentence_transformers import SentenceTransformer
+from qdrant_client import QdrantClient
+
+# Load environment variables
+load_dotenv(override=True)
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+QUADRANT_URL = os.getenv("QUADRANT_URL")
+QUADRANT_API_KEY = os.getenv("QUADRANT_API_KEY")
+
+
+# Initialize the LLM
+llm = ChatGroq(
+    model="qwen/qwen3-32b",
+    api_key=groq_api_key,
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+# Load the sentence transformer model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# Connect to Qdrant
+qdrant_client = QdrantClient(
+    url=QUADRANT_URL,
+    api_key=QUADRANT_API_KEY,
+)
+
+# Query
+query = "foods for anemia"
+
+query_vector = model.encode(query).tolist()
+
+results = qdrant_client.search(
+    collection_name="nutrition_rag",
+    query_vector=query_vector,
+    limit=3
+)
+
+for res in results:
+    print(res.payload["text"])
